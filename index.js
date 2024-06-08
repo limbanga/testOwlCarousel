4fae31474e641f55b1ade88f9c47bb236e2440d3
limbanga <95570833+limbanga@users.noreply.github.com>
2024-06-08 21:40:41 +0700

    add readme file

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ $(document).ready(function () {
     margin: 10,
     nav: true,
     dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
 });
